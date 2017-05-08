eeb3d9dd301c3c79e7c4ffcf8d181b834a1cd0de
keyserfaty <keserfaty@gmail.com>
2017-05-08 14:47:45 -0300

    add userUpdate module

--- src/modules/updates/components/UserUpdate/index.js
+++ src/modules/updates/components/UserUpdate/index.js
@@ -1,16 +1,32 @@
-import {html} from 'snabbdom-jsx';
+import xs from 'xstream'
+import { html } from 'snabbdom-jsx';
 
-const UserUpdate = sources => {
-  return (
+function intent (DOMSource) {
+  return {}
+}
+
+function model (events) {
+  return xs.combine().map(x => x)
+}
+
+function view (state$) {
+  return state$.map(state => (
     <div>
       <div className="update">
         <div className="image" />
         <div className="content">
+          <label>Add your update:</label>
           <input type="text"/>
         </div>
       </div>
     </div>
-  )
+  ))
+}
+
+function UserUpdate (sources) {
+  return {
+    DOM: view(model(intent(sources.DOM)))
+  }
 }
 
 export default UserUpdate
--- src/modules/updates/index.js
+++ src/modules/updates/index.js
@@ -2,15 +2,26 @@ import { span } from '@cycle/dom'
 import { html } from 'snabbdom-jsx';
 import xs from 'xstream'
 
-import Header from '../common/Header/index'
-//import UserUpdate from './components/UserUpdate'
+import Header from '../common/Header'
+import UserUpdate from './components/UserUpdate'
 //import Update from './components/Update'
 
 function main (sources) {
   const header = Header(sources)
+  const userUpdate = UserUpdate(sources)
 
-  const vtree$ = header.DOM
-    .map(dom => <span>{dom}</span>)
+  const items = xs.combine(
+    header.DOM,
+    userUpdate.DOM
+  )
+
+  const vtree$ = items
+    .map(([header, userUpdate]) => (
+      <span>
+        {header}
+        {userUpdate}
+      </span>
+    ))
 
   return {
     DOM: vtree$
